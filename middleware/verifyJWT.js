909abf0dcc1e48a8c1f424adb8749a4b065e7e91
Rauf Afzal <raufafzal453@gmail.com>
2024-09-23 22:11:15 +0500

    authorization implemented with custom middleware from scratch

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -3,16 +3,19 @@ require('dotenv').config();
 
 
 const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    if (!authHeader) return res.status(401);
-    console.log(authHeader);
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    if (!authHeader?.startsWith('Bearer ')) return res.status(401);
+    console.log(`Auth header is ${authHeader}`);
+    console.log(`Req header is ${JSON.stringify(req.headers)}`);
     const token = authHeader.split(' ')[1];
     jwt.verify(
         token,
         process.env.ACCESS_TOKEN_SECRET,
         (err, decoded) => {
             if (err) return res.status(401);
-            req.user = decoded.username;
+            req.user = decoded.UserInfo.username;
+            req.roles = decoded.UserInfo.roles;
+            console.log(`decoded user is ${req.user}`)
             next();
         }
 
